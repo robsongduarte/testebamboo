74df4a8f6d8c4a3a5db46346c139bb433c274ae3
robsonduarteg <robsonduarteg@gmail.com>
2019-05-02 14:21:19 -0300

    exclusão da pasta dist

--- dist/out-tsc/src/app/model/pair.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Pair = /** @class */ (function () {
-    function Pair() {
-    }
-    return Pair;
-}());
-exports.Pair = Pair;
-//# sourceMappingURL=pair.dto.js.map
